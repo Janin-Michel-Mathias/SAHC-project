aa4928073c29e6d6903b02e0fc6785682709b881
Simon <szhang0709@gmail.com>
2026-03-31 10:32:13 +0200

    Connect frontend to api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,36 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { useState } from "react"
 
 export default function Page() {
+  const [text, setText] = useState("")
+  async function fetchAdmin(): Promise<void> {
+    const data = await fetch(process.env.NEXT_PUBLIC_API_URL + "admin")
+    const text = await data.text();
+    setText(text);
+  }
+
+  async function fetchBookings(): Promise<void> {
+    const data = await fetch(process.env.NEXT_PUBLIC_API_URL + "bookings")
+    const text = await data.text();
+    setText(text);
+  }
+
+  async function fetchDashboard(): Promise<void> {
+    const data = await fetch(process.env.NEXT_PUBLIC_API_URL + "statistics")
+    const text = await data.text();
+    setText(text);
+  }
+
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
+        <Button onClick={fetchAdmin}>Administration</Button>
+        <Button onClick={fetchBookings}>Réservation</Button>
+        <Button onClick={fetchDashboard}>Dashboard</Button>
       </div>
+      <div>{text}</div>
     </div>
   )
 }
